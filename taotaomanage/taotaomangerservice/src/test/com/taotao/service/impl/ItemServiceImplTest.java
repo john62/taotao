a47ddacc7ec6664202f804e7f1fb581e894ed610
luwenjie1 <luwenjie1@ceair.com>
2018-03-21 10:10:01 +0800

    taotaomanage

---
+++ taotaomanage/taotaomangerservice/src/test/com/taotao/service/impl/ItemServiceImplTest.java
@@ -0,0 +1,33 @@
+//package com.taotao.service.impl;
+//
+//import com.taotao.mapper.TbItemMapper;
+//import com.taotao.pojo.TbItem;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+///**
+// * Created by 000290 on 2018/3/20.
+// */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:/spring/*.xml")
+//public class ItemServiceImplTest {
+//    @Autowired
+//    private TbItemMapper itemMapper;
+//
+//    @Test
+//    public void getItemById() throws Exception {
+//        TbItem item = itemMapper.selectByPrimaryKey(2);
+//        return item;
+//    }
+//
+//    @Test
+//    public void getItemList() throws Exception {
+//    }
+//
+//
+//
+//
+//}
